2d8caa30aba4e6f65a66a6a1dab09089f2a8b9d5
druva <druva@localhost.localdomain>
2025-05-14 17:09:23 +0530

    updated

--- To-do/scripts.js
+++ To-do/scripts.js
@@ -10,18 +10,21 @@
 
   function addTask() {
     const taskText = inputBox.value.trim();
-    if (!taskText) return;
+    if (!taskText) {
+      alert("please write down the task");
+      return;
+    };
 
     const task = document.createElement("div");
     task.className = "task";
 
     task.innerHTML = `
-    <div>
+    <div onclick="this.nextElementSibling.classList.toggle('show')">
     <div class="task-header" onclick="this.nextElementSibling.classList.toggle('show')">
-      <label><input type="checkbox"> ${taskText}</label>
-      <button onclick="editTask(button)">Edit</button>
-      <button onclick="this.closest('.task').remove()">Delete</button>
+      <label><input type="checkbox"> ${taskText}</label> 
       
+      <span class="arrow">&#9660;</span>
+          
     </div>
     
     <div class="task-body">
@@ -29,7 +32,7 @@
       <textarea placeholder="Write your notes..."></textarea>
 
       <label>Due Date</label>
-      <input type="date">
+      <input id="dt" type="date"><br>
 
       <label>Priority</label>
       <select>
@@ -37,34 +40,31 @@
         <option>Medium</option>
         <option>Low</option>
       </select>
+      
+      <button onclick="this.closest('.task').remove()">Delete</button>
 
       
     <div>
     </div>
+
+    
   `;
-   taskList.appendChild(task);
-    inputBox.value = "";
-    checkbox.addEventListener("change", function () {
-    if (this.checked) {
-      actionBar.style.display = "block";
-    } else {
-      actionBar.style.display = "none";
-    }
-});
-  }
-    function editTask(button) {
-    const task = button.closest('.task');
-    const label = task.querySelector('.task-header label');
-    const input = label.querySelector('input[type="checkbox"]');
-    const taskTextNode = label.childNodes[1];
-
-    const currentText = taskTextNode.nodeValue.trim();
-    const newText = prompt("Edit task:", currentText);
-
-    if (newText !== null && newText.trim() !== "") {
-      taskTextNode.nodeValue = " " + newText;
-    }
+  
+  taskList.appendChild(task);
+  inputBox.value = "";
+    
+  checkbox.addEventListener("click", function (){
+  taskList.classList.toggle("completed", checkbox.checked);
+    
+    
+  });
+  function clearAllTasks() {
+  const taskList = document.getElementById("task-list");
+  taskList.innerHTML = ""; // This clears all tasks
 }
+  
+}
+   
 
 
    
